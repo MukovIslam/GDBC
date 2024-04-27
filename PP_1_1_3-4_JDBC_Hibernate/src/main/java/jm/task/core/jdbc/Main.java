7bc08fc4996ed435c3ea69cd00020879c1e5dbce
Mucov Islam <islam310788@gmail.com>
2024-04-27 21:21:33 +0300

    "User с именем — name добавлен в базу данных" - прописано в методе service .
    лишняя строчка  удалена .

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/Main.java
@@ -19,13 +19,13 @@ public class Main {
     public static void main(String[] args) {
         userService1.createUsersTable();
         userService1.saveUser("Игорь", "Игорев", (byte) 23);
-        System.out.println("User с именем — Игорь добавлен в базу данных ");
+
         userService1.saveUser("Миша", "Михайлов", (byte) 20);
-        System.out.println("User с именем — Миша добавлен в базу данных ");
+
         userService1.saveUser("Жека", "Козлова", (byte) 22);
-        System.out.println("User с именем — Жека добавлен в базу данных ");
+
         userService1.saveUser("Люда", "Павлова", (byte) 25);
-        System.out.println("User с именем — Люда добавлен в базу данных ");
+
 
         for (User user : userService1.getAllUsers()) {
             System.out.println(user);
